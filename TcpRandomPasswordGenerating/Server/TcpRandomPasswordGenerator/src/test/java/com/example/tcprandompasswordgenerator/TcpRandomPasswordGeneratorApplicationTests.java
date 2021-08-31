3fc649ea4bfc0b959fe064e3c8a2162962eb4e03
gencdevops <gencdevops@gmail.com>
2021-08-31 22:36:59 +0300

    -

---
+++ TcpRandomPasswordGenerating/Server/TcpRandomPasswordGenerator/src/test/java/com/example/tcprandompasswordgenerator/TcpRandomPasswordGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tcprandompasswordgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcpRandomPasswordGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
